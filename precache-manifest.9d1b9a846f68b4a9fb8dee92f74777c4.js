90a9688ac01ffbb1250bf7e04eb7cbed42189831
Mastrianth <kholodnyack.n@yandex.ua>
2021-02-24 18:30:33 +0300

    Updates

---
+++ precache-manifest.9d1b9a846f68b4a9fb8dee92f74777c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9218ee8c62499995765483999f68568",
+    "url": "/sn/index.html"
+  },
+  {
+    "revision": "81da235b270af60ed471",
+    "url": "/sn/static/css/main.02d011ee.chunk.css"
+  },
+  {
+    "revision": "1a444b09387d1d2a78de",
+    "url": "/sn/static/js/2.a4341b38.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sn/static/js/2.a4341b38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81da235b270af60ed471",
+    "url": "/sn/static/js/main.ff5c0625.chunk.js"
+  },
+  {
+    "revision": "31892c667a5edea86959",
+    "url": "/sn/static/js/runtime-main.6a76cfd9.js"
+  },
+  {
+    "revision": "4d08320c0b95cfeadeb12a26de27bbdb",
+    "url": "/sn/static/media/preloader.4d08320c.svg"
+  },
+  {
+    "revision": "35c99376d9f15737601bf608fc771de6",
+    "url": "/sn/static/media/user.35c99376.jpg"
+  }
+]);
